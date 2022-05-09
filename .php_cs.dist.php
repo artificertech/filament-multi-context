ea4a0b03a7e916c44e4b10adb659f5b79454093f
Cole <cole.shirley@artificertech.com>
2022-05-09 11:00:03 -0600

    fix binary operator styling

--- .php_cs.dist.php
+++ .php_cs.dist.php
@@ -20,7 +20,7 @@ return (new PhpCsFixer\Config())
         'trailing_comma_in_multiline' => true,
         'phpdoc_scalar' => true,
         'unary_operator_spaces' => true,
-        'binary_operator_spaces' => true,
+        'binary_operator_spaces' => false,
         'blank_line_before_statement' => [
             'statements' => ['break', 'continue', 'declare', 'return', 'throw', 'try'],
         ],
