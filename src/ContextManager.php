19bb264e9bafdbc763018cb243a267ebba69c992
iotronlab <50877415+iotronlab@users.noreply.github.com>
2022-10-10 21:37:28 +0530

    Create ContextManager.php
    Added multi auth guard implementation.

---
+++ src/ContextManager.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Artificertech\FilamentMultiContext;
+
+use Filament\FilamentManager;
+use Illuminate\Contracts\Auth\Guard;
+
+class ContextManager extends FilamentManager
+{
+    protected static ?string $config = null;
+
+    public function __construct($config)
+    {
+        static::$config = $config;
+    }
+    public static function getAuth(): ?Guard
+    {
+        return static::$config ? auth()->guard(config(static::$config . '.auth.guard')) : null;
+    }
+
+    public function auth(): Guard
+    {
+        return static::getAuth() ?? auth()->guard(config('filament.auth.guard'));
+    }
+}
